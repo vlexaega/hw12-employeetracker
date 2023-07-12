e2f746d9d8dbdfdf8d15aac6326219c3417734e3
vlexaega <132007323+vlexaega@users.noreply.github.com>
2023-07-12 09:36:22 -0400

    adds the function to start the user prompts

--- index.js
+++ index.js
@@ -4,8 +4,73 @@ const mysql = require ("mysql2");
 const cTable = require('console.table');
 
 //CONNECT TO DATABASE
+const db = mysql.createConnection({
+    host: "localhost",
+    port: 3301,
+    user: "root",
+    password: "alligator",
+    database: "emp_db"
+});
+
+db.connect(function(err) {
+    if(err) throw err
+    console.log("Connect to emp_db")
+    startPrompts();
+});
 
 //FUNCTION TO PROMPT USER FOR ACTION
+function startPrompts(){
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            name: "choice",
+            choices: [
+                "View all employees",
+                "View all roles",
+                "View all departments",
+                "Update employee",
+                "Add employee",
+                "Add role",
+                "Add department",
+            ]
+        }
+    ]).then(function(val) {
+        switch (val.choice) {
+            case "View All Employees":
+                viewAllEmployees();
+            break;
+
+            case "View All Roles":
+                viewAllRoles();
+            break;
+
+            case "View All Departments":
+                viewAllDepartments();
+            break;
+
+            case "Update Employee":
+                updateEmployee();
+            break;
+
+            case "Add Employee":
+                addEmployee();
+            break;
+
+            case "Add Role":
+                addRole();
+            break;
+
+            case "Add Department":
+                addDepartment();
+            break;
+
+            case "View Employees by Manager":
+                viewEmployeesByManager();
+            break;
+        }
+    })
+}
 
 //FUNCTION TO VIEW ALL EMPLOYEES
 
