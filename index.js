037078409de4056d53fc97782fa0a64344e1c88d
vlexaega <132007323+vlexaega@users.noreply.github.com>
2023-07-12 09:37:30 -0400

    adds function to view employees

--- index.js
+++ index.js
@@ -73,6 +73,14 @@ function startPrompts(){
 }
 
 //FUNCTION TO VIEW ALL EMPLOYEES
+function viewAllEmployees(){
+    db.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id LEFT JOIN employee e on employee.manager_id = e.id;", 
+    function(err, res) {
+      if (err) throw err
+      console.table(res)
+      startPrompts()
+  })
+}
 
 //FUNCTION TO VIEW ALL EMPLOYEES BY ROLES
 
